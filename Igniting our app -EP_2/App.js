f04203e3d39b50701b44edf127731cf86aa90fe4
rajmahajan98 <47290275+rajmahajan98@users.noreply.github.com>
2024-09-25 17:21:00 +0530

    Adding Files

---
+++ Igniting our app -EP_2/App.js
@@ -0,0 +1,3 @@
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        React.createElement("h1", {}, "I'm an H1 Tag")));
---
+++ Inception - EP_1/App.js
@@ -0,0 +1,18 @@
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World From React");
+
+// const root = ReactDom.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// Nested React Elements
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        React.createElement("h1", {}, "I'm an H1 Tag")));
+
+// If we have multiple Sibling or Child Elements we need to Use Array or List.
+
+
+
+
+
